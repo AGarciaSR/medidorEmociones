eef7bf942c3e9dc0e74ff0ce0d04d346d429bad2
Alberto <agarciablasco2@gmail.com>
2023-04-04 15:47:39 -0400

    Nuevas condiciones para responsividad de tabla

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
     readjustSizes();
+    $("td").click(function (e) { 
+        e.preventDefault();
+        seleccionaCelda(this.id);
+    });
 });
 $(window).resize(function() {
     readjustSizes();
 });
 function readjustSizes(){
-    $("#ejeY").height($("#ejeX").width() / 1.7);
+    let proportionScreen = $("body").width() / $("body").height();
+    let newDivision = (proportionScreen * 1.7) / 1.93;
+    if($("body").width()>768)
+        $("#ejeY").height($("#ejeX").width() / newDivision);
+    else
+        $("#ejeY").height($("#ejeX").width() / 1);
+}
+function seleccionaCelda(id){
+    $("td").removeClass("selected");
+    $("td#"+id).addClass("selected");
 }
