ed701828526f0190564be73447789ec53f9ad5f4
Alberto García <agarciablasco2@gmail.com>
2023-04-11 19:45:42 -0400

    Crea lista emociones y función que muestra
    Se añade la función que mostrará la emoción elegida en el cuadro de colores.

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
+var celdaElegida;
+var emociones = ["Rabioso","En pánico","Estresado","Agitado","Conmocionado","Sorprendido","Alentado","Festivo","Regocijado","Extasiado","Colérico","Furioso","Frustrado","Tenso","Aturdido","Hiperactivo","Jovial","Motivado","Inspirado","Exaltado","Echando humo","Atemorizado","Enojado","Nervioso","Inquieto","Energético","Animado","Entusiasmado","Optimista","Excitado","Ansioso","Aprensivo","Preocupado","Irritado","Molesto","Complacido","Feliz","Enfocado","Orgulloso","Emocionado","Repulsivo","Emproblemado","Intranquilo","Difícil","Enfurecido","Agradable","Alegre","Esperanzado","Juguetón","Dichoso","Disgustado","Sombrío","Decepcionado","Abatido","Apático","A gusto","Acomodadizo","Contento","Amoroso","Realizado","Pesimista","Malhumorado","Desanimado","Triste","Aburrido","Calmado","Seguro","Satisfecho","Agradecido","Conmovido","Enajenado","Miserable","Solitario","Descorazonado","Cansado","Relajado","Fresco","Descansado","Bendecido","Equilibrado","Desalentado","Deprimido","Hosco","Exhausto","Fatigativo","Suave","Pensativo","En paz","Cómodo","Despreocupado","Desesperado","Desesperanzado","Desolado","Agotado","Drenado","Soñoliento","Complaciente","Tranquilo","Acogedor","Sereno"];
 $(document).ready(function() {
-    var celdaElegida;
     readjustSizes();
     $("td").click(function (e) { 
         e.preventDefault();
@@ -23,5 +24,6 @@ function seleccionaCelda(id){
     $("#contLogoMedidor button").removeClass("visually-hidden");
     $("td#"+id).addClass("selected");
 }
-function enviaEmocion(){ 
+function enviaEmocion(){
+    $("#nombre").html(emociones[celdaElegida.substring(4)-1])
 }
