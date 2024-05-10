c0793a375423b65a6a51316448780ab4241de0e9
Alberto García <48536830+AGarciaSR@users.noreply.github.com>
2024-05-10 18:36:59 +0200

    Redistribución y adición de contenido
    - Se redistribuyen los logos, quedando en una fila, superior a la tabla de opciones.
    - Añadidos datos sobre descripciones, funciones y consejos de cada emoción, que se pueden visualizar mediante opciones en el modal que aparece al seleccionar un cuadro.

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,13 @@
 var celdaElegida;
-var emociones = ["Rabioso","En pánico","Estresado","Agitado","Conmocionado","Sorprendido","Alentado","Festivo","Regocijado","Extasiado","Colérico","Furioso","Frustrado","Tenso","Aturdido","Hiperactivo","Jovial","Motivado","Inspirado","Exaltado","Echando humo","Atemorizado","Enojado","Nervioso","Inquieto","Energético","Animado","Entusiasmado","Optimista","Excitado","Ansioso","Aprensivo","Preocupado","Irritado","Molesto","Complacido","Feliz","Enfocado","Orgulloso","Emocionado","Repulsivo","Emproblemado","Intranquilo","Difícil","Enfurecido","Agradable","Alegre","Esperanzado","Juguetón","Dichoso","Disgustado","Sombrío","Decepcionado","Abatido","Apático","A gusto","Acomodadizo","Contento","Amoroso","Realizado","Pesimista","Malhumorado","Desanimado","Triste","Aburrido","Calmado","Seguro","Satisfecho","Agradecido","Conmovido","Enajenado","Miserable","Solitario","Descorazonado","Cansado","Relajado","Fresco","Descansado","Bendecido","Equilibrado","Desalentado","Deprimido","Hosco","Exhausto","Fatigativo","Suave","Pensativo","En paz","Cómodo","Despreocupado","Desesperado","Desesperanzado","Desolado","Agotado","Drenado","Soñoliento","Complaciente","Tranquilo","Acogedor","Sereno"];
+var celdasOn = 0;
 $(document).ready(function() {
     readjustSizes();
     $("td").click(function (e) { 
         e.preventDefault();
         seleccionaCelda(this.id);
+        enviaEmocion();
     });
+    $('#modalEmocion').modal({ show: false});
 });
 $(window).resize(function() {
     readjustSizes();
@@ -20,25 +22,65 @@ function readjustSizes(){
         $("#textHorizontal").addClass("hidden");
         let proportionScreen = $(window).width() / $(window).height();
         let newDivision = (proportionScreen * 1.9) / 2.1;
-        if($(window).width()>768)
+        if($(window).width()>600)
             $("#ejeY").height($(window).height() / 1.216);
         else
             $("#ejeY").height($("#ejeX").width() / 1);
+        if($(window).height()<600){
+            var celda = 1;
+            for(var i=0;i<emociones.length;i++){
+                $("#cell"+celda+" br").css("display","none");
+                celda++;
+            }
+        }
+        else{
+            var celda = 1;
+            for(var i=0;i<emociones.length;i++){
+                $("#cell"+celda+" br").css("display","block");
+                celda++;
+            }
+        }
     }
 }
 function seleccionaCelda(id){
-    celdaElegida = id
+    celdaElegida = id;
     $("td").removeClass("selected");
     $("#contLogoMedidor button").removeClass("visually-hidden");
     $("td#"+id).addClass("selected");
 }
 function enviaEmocion(){
-    $("#nombre").html(emociones[celdaElegida.substring(4)-1]);
-    if($(window).width()>768){
-        var celda = 1;
-        for(var i=0;i<emociones.length;i++){
-            $("#cell"+celda).html(emociones[i]);
-            celda++;
+    $("#nombre").html(emociones[celdaElegida]);
+    $("#causa-texto").html(causas[celdaElegida]);
+    $("#funcion-texto").html(funciones[celdaElegida]);
+    $("#consejos-texto").html(consejos[celdaElegida]);
+    $(".enablers .btn").removeClass("active");
+    $("#part1enabler .btn").addClass("active");
+    $("#part1,#part2,#part3").hide();
+    $("#part1").show();
+    $("#modalEmocion").show();
+    $(".enablers").height($("#enablersContainer").height());
+    $("#modalEmocion").hide();
+    $('#modalEmocion').modal('show');
+}
+function revelaCeldas(){
+    if(celdasOn == 0){
+        celdasOn = 1;
+        $("#eyeCells").attr("src","./img/eye.png").attr("title","Haz click para ocultar los nombres de las emociones");
+        for(var celda = 0;celda <= Object.keys(emociones).length;celda++){
+            $("#cell"+celda).html(emociones["cell"+celda]);
         }
     }
+    else{
+        celdasOn = 0;
+        $("#eyeCells").attr("src","./img/eye_closed.png").attr("title","Haz click para ver los nombres de las emociones");
+        for(var celda = 0;celda <= Object.keys(emociones).length;celda++){
+            $("#cell"+celda).html("<br>");
+        }
+    }
+}
+function showPart(part){
+    $("#part1,#part2,#part3").hide();
+    $(".enablers .btn").removeClass("active");
+    $("#part"+part+"enabler .btn").addClass("active");
+    $("#part"+part).show();
 }
